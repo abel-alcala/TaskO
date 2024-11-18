9b4b6cd7c502cc05f783f9974dd0be326f2624e7
ssasukek <kevinlu3234@gmail.com>
2024-11-17 21:08:33 -0800

    fix?

--- packages/express-backend/server.js
+++ packages/express-backend/server.js
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
     res.send("Welcome to the API!");
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
